fb6aa01059ffbecd4b6855a96a2f9952f21f40d0
David Florez <dflorez6@gmail.com>
2024-07-22 00:27:18 -0400

    v1.0.2
    ## Back-End ##
    * Configuring back4app deployment
    
    ## Pending ##
    *
    
    # CTWR | WEBSITE v2 #

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Allow requests from the specified host (back4app)
+  config.hosts << "ctwrnewwebsite-i27r94pz.b4a.run"
 end
